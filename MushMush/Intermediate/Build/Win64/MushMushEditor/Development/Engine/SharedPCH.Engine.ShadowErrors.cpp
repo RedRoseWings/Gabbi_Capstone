02310bdd8806cc5d137d334bd3df5764142edaef
James Justice <lordjaidon@gmail.com>
2022-03-17 19:44:49 -0400

    Added a couple sounds, music triggers, music fade, worked in J_Sound map for audio implementations for now.

--- MushMush/Intermediate/Build/Win64/MushMushEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ MushMush/Intermediate/Build/Win64/MushMushEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
 // Compiler: 14.29.30140
-#include "D:/miles/Documents/Unreal Projects/Gabbi_Capstone/MushMush/Intermediate/Build/Win64/MushMushEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "F:/Documents/Unreal/MushMush/Intermediate/Build/Win64/MushMushEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
